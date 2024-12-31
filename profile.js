fbb456ecc449698901be563a389232487f4fb95f
degujoe <129337742+degujoe@users.noreply.github.com>
2024-12-31 14:41:08 +0000

    Update profile.js

--- profile.js
+++ profile.js
@@ -66,11 +66,8 @@ document.addEventListener('DOMContentLoaded', function () {
             ratingDiv.innerHTML = `
               <p><strong>By:</strong> ${rating.rating_by}</p>
               <p><strong>Date:</strong> ${rating.rating_date}</p>
-              <p><strong>Overall Rating:</strong> ${rating.overall_rating}</p>
-              <p><strong>Physical Rating:</strong> ${rating.physical_score}</p>
+              <p><strong>Rating Description:</strong>
               <p>${rating.rating_description}</p>
-              <p><strong>Personality Rating:</strong> ${rating.personality_score}</p>
-              <p>${rating.personality_description}</p>
             `;
             ratingsSection.appendChild(ratingDiv);
           });
