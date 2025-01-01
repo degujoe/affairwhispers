dabe80b7a780989573911152a740f6fa0da45db3
degujoe <129337742+degujoe@users.noreply.github.com>
2025-01-01 15:20:37 +0000

    Update profile.js

--- profile.js
+++ profile.js
@@ -92,7 +92,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 async function checkMembership(email) {
   try {
-    const response = await fetch('https://ebde-86-160-46-121.ngrok-free.app/check-subscription', {
+    const response = await fetch('https://99c9-86-160-46-121.ngrok-free.app/check-subscription', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email }),
