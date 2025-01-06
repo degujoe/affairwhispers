ee86350beb7bf7f37b0c81bd904e8c504b97954d
degujoe <129337742+degujoe@users.noreply.github.com>
2025-01-06 16:45:30 +0000

    Update profile.js

--- profile.js
+++ profile.js
@@ -210,7 +210,7 @@ async function showSubscriptionPopup() {
             <li><strong>Contact Feature Unlocked:</strong> Directly connect with all profiles.</li>
             <li><strong>Discreet:</strong> Payment appears as AMZNMKTPLACE (same as purchasing from Amazon).</li>
             <li><strong>No Hidden Fees:</strong> Transparent pricing with no surprises.</li>
-            <li><strong>Great Value:</strong> All for just <strong>£30!</strong></li>
+            <li><strong>Free Trial:</strong><strong>7-Day free trial</strong></li>
         </ul>
         <p>Please log in or create an account to proceed:</p>
         <button onclick="redirectToLogin()" class="btn-primary">Log In / Sign Up</button>
